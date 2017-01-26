3560c60b8d2c5042794ed2a5612a95f27df518c5
a0123055u <festecole06>
2017-01-26 23:15:28 +0800

    Changed the server.js for OpenShift project.

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var db = mongojs('contactlist',['contactlist']);
 var bodyParser= require('body-parser');
 
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 7557
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+
+
+
 app.use(express.static(__dirname+ "/public"));
 //app.use(bodyParser.json());
 app.use(express.json());
@@ -79,6 +85,9 @@ app.put('/contaclist/:id',function(req,res){
                                     res.json(doc);
     });
 });
-    
-app.listen(3000);
+
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});    
+
 console.log('Server running in port 3000');
