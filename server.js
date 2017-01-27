f6d052533efce34bcb518341cdd9920209cdb83b
a0123055u <festecole06>
2017-01-27 15:56:04 +0800

    Used MongoClient inside Get,Post,Put and Delete methods in Server.js for Hosting in OpenShit
    Signed-off-by: a0123055u <festecole06>

--- server.js
+++ server.js
@@ -21,14 +21,26 @@ if(process.env.OPENSHIFT_MONGODB_DB_URL){
 app.use(express.static(__dirname+ "/public"));
 //app.use(bodyParser.json());
 app.use(express.json());
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Connected to Database");
-
+var dbCollection;
 
 app.get('/contactlist',function(req,res){
     console.log('recieved a get');
+	
+	MongoClient.connect(url, function(err, db1) {
+  if (err) throw err;
+  console.log("Connected to Database");
+  
+
+db.contactlist.find(function(err,docs){
+        console.log(docs);
+        res.json(docs);
+		db1.close()
+    });
+
+
+});
+
+});
 
     
 // //   trying push data from server to front end as static test
@@ -50,50 +62,72 @@ app.get('/contactlist',function(req,res){
     
     // // Accessing data from mongo db
     
-    db.contactlist.find(function(err,docs){
+    /* db.contactlist.find(function(err,docs){
         console.log(docs);
-        res.json(docs);
-    });
-});
+        res.json(docs); */
+    //});
 
 app.post('/contactlist',function(req,res){
    // console.log(req.body);
-    var val ={name:req.body.name, email:req.body.email,number:req.body.number};
-   //var list = db.collection('contactlist');
-    db.contactlist.insert(val, function(err,res){
-        if(!err)
-            console.log("Inserted into Db");
-    });
+	   MongoClient.connect(url, function(err, db1) {
+	  if (err) throw err;
+	  console.log("Connected to Database");
+			var val ={name:req.body.name, email:req.body.email,number:req.body.number};
+		   //var list = db.collection('contactlist');
+			db.contactlist.insert(val, function(err,res){
+				if(!err)
+					console.log("Inserted into Db");
+					
+					db1.close();
+			});
     
     
+});
 });
 
 app.delete('/contactlist/:id',function(req,res){
-    var id = req.params.id;
-    console.log(id);
-    db.contactlist.remove({_id:mongojs.ObjectId(id)},function(err,doc){
-        res.json(doc);
-        
+	
+	
+	 MongoClient.connect(url, function(err, db1) {
+	  if (err) throw err;
+	  console.log("Connected to Database");
+
+					var id = req.params.id;
+					console.log(id);
+					db.contactlist.remove({_id:mongojs.ObjectId(id)},function(err,doc){
+						res.json(doc);
+						db1.close();
     
     });
 });
-
-app.get('/contactlist/:id',function(req,res){
-    var id = req.params.id;
-    console.log(id);
-    db.contactlist.findOne({_id:mongojs.ObjectId(id)},function(err,doc){
-        console.log(doc);
-        res.json(doc);
-    });
 });
+app.get('/contactlist/:id',function(req,res){
+    MongoClient.connect(url, function(err, db1) {
+	  if (err) throw err;
+	  console.log("Connected to Database");
 
+
+			   var id = req.params.id;
+				console.log(id);
+				db.contactlist.findOne({_id:mongojs.ObjectId(id)},function(err,doc){
+					console.log(doc);
+					res.json(doc);
+					db1.close();
+				});
+});
+});
 app.put('/contaclist/:id',function(req,res){
-    var id= req.params.id;
-    console.log("Name in server"+req.body.name);
-    db.contactlist.findAndModify({query:{_id:mongojs.ObjectId(id)},
-                                 update:{$set:{name:req.body.name,email:req.body.email,number:req.body.number}},
-                                 new: true},function(err,doc){
-                                    res.json(doc);
+
+	 MongoClient.connect(url, function(err, db1) {
+	  if (err) throw err;
+	  console.log("Connected to Database");
+				var id= req.params.id;
+				console.log("Name in server"+req.body.name);
+				db.contactlist.findAndModify({query:{_id:mongojs.ObjectId(id)},
+											 update:{$set:{name:req.body.name,email:req.body.email,number:req.body.number}},
+											 new: true},function(err,doc){
+												res.json(doc);
+												db1.close();
     });
 });
 });
